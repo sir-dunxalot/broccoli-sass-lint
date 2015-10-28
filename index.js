8d90dcc990b55b8d069807e3efe56e19996c9e51
Duncan Graham Walker <walkerdu@bc.edu>
2015-10-28 19:25:50 -0400

    Using specifiable configPath

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 'use strict';
 
 var Filter = require('broccoli-filter');
+var chalk = require('chalk');
+var findupSync = require('findup-sync');
 var linter = require('sass-lint');
 var path = require('path');
 
+var fileOptions = { encoding: 'utf8' };
+
 SassLinter.prototype = Object.create(Filter.prototype);
 SassLinter.prototype.constructor = SassLinter;
+
+function stripComments(string) {
+  string = string || '';
+
+  string = string.replace(/\/\*(?:(?!\*\/)[\s\S])*\*\//g, '');
+  string = string.replace(/\/\/[^\n\r]*/g, ''); // Everything after '//'
+
+  return string;
+};
 
 function SassLinter(inputTree, options) {
   if (!(this instanceof SassLinter)) {
@@ -17,7 +30,6 @@ function SassLinter(inputTree, options) {
   Filter.call(this, inputTree, options);
 
   this.inputTree = inputTree;
-  this._errors = [];
 
   if (options.failOnError === false) {
     linter.failOnError = function() {};
@@ -37,22 +49,61 @@ SassLinter.prototype.targetExtension = 'scss';
 
 SassLinter.prototype.build = function() {
   var _this = this;
+  var lintingConfig;
+
+  this._errors = [];
+
+  if (!this.configPath) {
+    this.configPath = 'sass-lint.yml';
+  }
+
+  if (!this.lintingConfig) {
+    lintingConfig = this.getConfig();
+
+    if (lintingConfig) {
+      this.lintingConfig = lintingConfig;
+    }
+  }
 
   return Filter.prototype.build.call(this).finally(function() {
     var errors = _this._errors;
 
-    if (errors.length && !this.silence) {
+    if (errors.length && !_this.silence) {
       console.log(linter.format(errors)); // Display errors
     }
   });
 }
 
+SassLinter.prototype.getConfig = function(rootPath) {
+  var configPath;
+
+  if (!rootPath) {
+    rootPath = process.cwd();
+  }
+
+  configPath = findupSync(this.configPath, {
+    cwd: rootPath,
+    nocase: true,
+  });
+
+  if (configPath) {
+    try {
+      return linter.getConfig({}, configPath);
+    } catch (error) {
+      this.console.error(chalk.red('Error occured parsing sass-lint.yml'));
+      this.console.error(error.stack);
+
+      return null;
+    }
+  }
+}
+
 SassLinter.prototype.processString = function(content, relativePath) {
   var lint = linter.lintText({
-    'text': content,
-    'format': path.extname(relativePath).replace('.', ''),
-    'filename': relativePath
-  }, this.lintingOptions);
+    text: content,
+    format: path.extname(relativePath).replace('.', ''),
+    filename: relativePath
+  }, this.lintingConfig);
 
   if (lint.errorCount || lint.warningCount) {
     this.logError(lint);
--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var SassLinter = require('..');
-var assert = require('assert');
+// var assert = require('assert');
 var broccoli = require('broccoli');
+var chai = require('chai');
 var fs = require('fs');
 var linter = require('sass-lint');
+
+var assert = chai.assert;
 
 var builder, errors;
 
@@ -24,11 +27,12 @@ describe('broccoli-sass-hint', function() {
     }
   });
 
-  it('The linter should catch errors', function() {
+  it('The linter should use a config file', function() {
     var sourcePath = 'tests/fixtures';
 
     var node = new SassLinter(sourcePath, {
       logError: function(fileLint) { errors.push(fileLint) },
+      configPath: 'sass-lint-test-config.yml',
       silence: true,
     });
 
@@ -36,7 +40,26 @@ describe('broccoli-sass-hint', function() {
 
     return builder.build().then(function() {
 
-      assert.ok(linter.format(errors).indexOf('Extends should come before declarations') > -1,
+      assert.notInclude(linter.format(errors), 'ID selectors not allowed',
+        'Should respect non-default rules specified in project\'s sass-lint.yml');
+
+    });
+  });
+
+  it('The linter should catch errors', function() {
+    var sourcePath = 'tests/fixtures';
+
+    var node = new SassLinter(sourcePath, {
+      logError: function(fileLint) { errors.push(fileLint) },
+      configPath: 'sass-lint-test-config.yml',
+      silence: true,
+    });
+
+    builder = new broccoli.Builder(node);
+
+    return builder.build().then(function() {
+
+      assert.include(linter.format(errors), 'Extends should come before declarations',
         'Should pick up linting error based on order');
 
     });
