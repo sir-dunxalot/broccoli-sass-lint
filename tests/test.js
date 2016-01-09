c010079d1e75b8fb839a4dcf5a0ea96444a9cdad
Christopher Garrett <cmg@pzuraq.co>
2016-01-09 11:04:00 -0800

    Adds PhantomJS test generators so that tests will fail on lint failures

--- tests/test.js
+++ tests/test.js
@@ -7,16 +7,27 @@ var fs = require('fs');
 var linter = require('sass-lint');
 
 var assert = chai.assert;
+var expect = chai.expect;
 
-var builder, errors;
+var tree, builder, errors;
 
-function buildAndLint(sourcePath) {
-  var node = new SassLinter(sourcePath, {
-    logError: function(fileLint) { errors.push(fileLint) },
-    configPath: 'sass-lint-test-config.yml', // So sass-lint.yml isn't caught instead of project's version
-  });
+function readFile(path) {
+  return fs.readFileSync(path, { encoding: 'utf8' });
+}
 
-  builder = new broccoli.Builder(node);
+function chdir(path) {
+  process.chdir(path);
+}
+
+function buildAndLint(sourcePath, options) {
+  options = options || {};
+
+  options.logError = function(fileLint) { errors.push(fileLint) };
+  options.configPath = 'sass-lint-test-config.yml'; // So sass-lint.yml isn't caught instead of project's version
+
+  tree = new SassLinter(sourcePath, options);
+
+  builder = new broccoli.Builder(tree);
 
   return builder.build();
 }
@@ -34,7 +45,7 @@ describe('broccoli-sass-hint', function() {
   });
 
   it('The linter should use a config file', function() {
-    return buildAndLint('tests/fixtures').then(function() {
+    return buildAndLint('tests/fixtures/all-sorts-of-issues').then(function() {
 
       assert.notInclude(linter.format(errors), 'ID selectors not allowed',
         'Should respect non-default rules specified in project\'s sass-lint.yml');
@@ -43,11 +54,43 @@ describe('broccoli-sass-hint', function() {
   });
 
   it('The linter should catch errors', function() {
-    return buildAndLint('tests/fixtures').then(function() {
+    return buildAndLint('tests/fixtures/all-sorts-of-issues').then(function() {
 
       assert.include(linter.format(errors), 'Extends should come before declarations',
         'Should pick up linting error based on order');
 
+    });
+  });
+
+  describe('testGenerator', function() {
+    it('retains targetExtension when disableTestGenerator is true', function() {
+      buildAndLint('tests/fixtures/no-issues-found', {
+        disableTestGenerator: true
+      }).then(function(results) {
+        expect(tree.targetExtension).to.eql('scss');
+      });
+    });
+
+    it('sets targetExtension correctly when disableTestGenerator is false', function() {
+      buildAndLint('tests/fixtures/no-issues-found').then(function(results) {
+        expect(tree.targetExtension).to.eql('sass-lint-test.js');
+      });
+    });
+
+    it('does not generate tests if disableTestGenerator is set', function() {
+      buildAndLint('tests/fixtures/no-issues-found', {
+        disableTestGenerator: true
+      }).then(function(results) {
+        var dir = results.directory;
+        expect(readFile(dir + '/index.' + tree.targetExtension)).to.not.match(/ok\(true, 'app.scss should pass sass lint.'\);/);
+      });
+    });
+
+    it('generates test files for sass issues', function() {
+      buildAndLint('tests/fixtures/no-issues-found').then(function(results) {
+        var dir = results.directory;
+        expect(readFile(dir + '/index.' + tree.targetExtension)).to.match(/ok\(true, 'index.js should pass sass lint.'\);/);
+      });
     });
   });
 });
